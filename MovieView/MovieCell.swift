608d05e492cd7196cfb9c57810d666c95cdaa9b8
Dominique Macias <dmacias@fb.com>
2016-06-16 23:42:46 -0700

    first commit

---
+++ MovieView/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieView
+//
+//  Created by Dominique Macias on 6/15/16.
+//  Copyright © 2016 Dominique Macias. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
