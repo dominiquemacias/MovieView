479e515aa37437aed6f53597b11f216af37b789b
Dominique Macias <dmacias@fb.com>
2016-06-17 13:28:43 -0700

    Initial commit

--- MovieView/MoviesViewController.swift
+++ MovieView/MoviesViewController.swift
@@ -7,13 +7,62 @@
 //
 
 import UIKit
+import AFNetworking
+import MBProgressHUD
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var movies: [NSDictionary]?
+    var filteredData: [NSDictionary]?
+    var endpoint: String!
+    
+    func networkRequest() {
+        let apiKey = "e1a568f5f99d0189c1e600392e0c19d9"
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
+        let request = NSURLRequest(
+            URL: url!,
+            cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
+            timeoutInterval: 10)
+        
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate: nil,
+            delegateQueue: NSOperationQueue.mainQueue()
+        )
+        
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                            data, options:[]) as? NSDictionary {
+                                print("response: \(responseDictionary)")
+                                    self.movies = responseDictionary["results"] as? [NSDictionary]
+                                        self.filteredData = self.movies
+                                        self.tableView.reloadData()
+                }
+            }
+        })
+        MBProgressHUD.hideHUDForView(self.view, animated: true)
+        task.resume()
+        
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        searchBar.delegate = self
+        
+        //Initialize a UIRefreshControl
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+        
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        self.networkRequest()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +70,94 @@ class MoviesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let filteredData = filteredData {
+            return filteredData.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
+        let movie = filteredData![indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        let baseUrl = "http://image.tmdb.org/t/p/w500"
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
 
-    /*
+        if let posterPath = movie["poster_path"] as? String {
+            let imageUrl = NSURL(string: baseUrl + posterPath)
+            cell.posterView.setImageWithURL(imageUrl!)
+        }
+        
+        //print("row \(indexPath.row)")
+        return cell
+    }
+    
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        self.networkRequest()
+        self.tableView.reloadData()
+        refreshControl.endRefreshing()
+        
+    }
+    
+
+    // This method updates filteredData based on the text in the Search Box
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        if searchText.isEmpty {
+            filteredData = movies
+        } else {
+            // The user has entered text into the search box
+            // Use the filter method to iterate over all items in the data array
+            // For each item, return true if the item should be included and false if the
+            // item should NOT be included
+            filteredData = movies!.filter({(dataItem: NSDictionary) -> Bool in
+                // If dataItem matches the searchText, return true to include it
+                let title = dataItem["title"] as! String
+                if title.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil {
+                    return true
+                } else {
+                    return false
+                }
+            })
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    
+
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPathForCell(cell)
+        let movie = filteredData![indexPath!.row]
+        
+        let detailViewController = segue.destinationViewController as! DetailViewController
+        detailViewController.movie = movie
+        
+        print("prepareForSegue Called")
+        
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+
     }
-    */
+    
 
 }
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment this line to define a global platform for your project
+  platform :ios, '9.0'
+
+target 'MovieView' do
+    pod 'AFNetworking'
+    pod 'MBProgressHUD'
+ 
+
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MovieView
+  inhibit_all_warnings!
+
+end
